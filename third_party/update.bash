f1161ff295e431ef77f4a927b4c165134ed1eb18
Joel Walker <joelwalker1995@gmail.com>
2022-05-20 13:30:27 -0700

    Fixed some build problems

--- third_party/update.bash
+++ third_party/update.bash
@@ -8,9 +8,10 @@ pushd `dirname $0` > /dev/null
 
 rm -rf uWebSockets readerwriterqueue
 
-git clone --recursive git@github.com:uNetworking/uWebSockets.git
+git clone git@github.com:uNetworking/uWebSockets.git
 pushd uWebSockets
 git checkout 7956546836904402ad18a00a1aa033c7a9c5c5ab
+git submodule update --init --recursive
 # git apply ../uWebSockets.patch
 pushd uSockets
 make -j8
